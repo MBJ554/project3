60b04d94c1ea96178989bbb87735afa0034d50b9
Mikkel Brøgger <mikkelbrgger@gmail.com>
2020-11-03 17:10:28 +0100

    Added API GetAll for City and Post for City, Customer work in progress

---
+++ API/Controllers/CustomerController.cs
@@ -0,0 +1,66 @@
+﻿using API.Models;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        private readonly string connectionString = ConfigurationManager.ConnectionStrings["connStr"].ConnectionString;
+        // GET: api/Customer
+        public IEnumerable<Customer> GetAll()
+        {
+            using (var conn = new SqlConnection(connectionString))
+            {
+                string sql = "SELECT * FROM Person";
+
+                return conn.Query<Customer>(sql);
+            }
+        }
+
+        // GET: api/Customer/5
+        public string GetById(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Customer
+        public void Post([FromBody]Customer customer)
+        {
+            using (var conn = new SqlConnection(connectionString))
+            {
+                string sql = "INSERT INTO [dbo].[Person] " +
+                    "([type], " +
+                    "[clinicId], " +
+                    "[practitionerId], " +
+                    "[rehabProgramId]," +
+                    "[firstName]," +
+                    "[lastName]," +
+                    "[phoneNo]," +
+                    "[email]," +
+                    "[password]," +
+                    "[address]," +
+                    "[zipCode])" +
+                    "VALUES (@type, @clinicId, @practitionerId, @rehabProgramId, @firstName, @lastName, @phoneNo, @email, @password, @address, @zipCode)";
+                conn.Execute(sql, customer);
+            }
+        }
+
+        // PUT: api/Customer/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Customer/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ API/Models/Customer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+	public class Customer
+	{
+		public int Id { get; set; }
+		public int TypeId { get; set; }
+		public int ClinicId { get; set; }
+		public int PractitionerId { get; set; }
+		public int RehabProgramId { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string PhoneNo { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public string Address { get; set; }
+		public string ZipCode { get; set; }
+
+		public Customer(int id_, int typeId_, int clinicId_, int practitionerId_, int rehabProgramId_, string firstName_, string lastName_, string phoneNo_, string email_, string password_, string address_, string zipCode_)
+		{
+			this.Id = id_;
+			this.TypeId = typeId_;
+			this.ClinicId = clinicId_;
+			this.PractitionerId = practitionerId_;
+			this.RehabProgramId = rehabProgramId_;
+			this.FirstName = firstName_;
+			this.LastName = lastName_;
+			this.PhoneNo = phoneNo_;
+			this.Email = email_;
+			this.Password = password_;
+			this.Address = address_;
+			this.ZipCode = zipCode_;
+		}
+        public Customer()
+        {
+
+        }
+	}
+}
