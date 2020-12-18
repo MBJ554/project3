fdece09178fca55b289dbb0dc9cb0f097ad82c8f
Mikkel Brøgger <mikkelbrgger@gmail.com>
2020-12-18 12:36:06 +0100

    beauty

--- API/DAL/Interfaces/IGenericRepository.cs
+++ API/DAL/Interfaces/IGenericRepository.cs
@@ -5,13 +5,9 @@ namespace API.DAL.Interfaces
     public interface IGenericRepository<TEntity>
     {
         IEnumerable<TEntity> GetAll();
-
         TEntity GetById(int id);
-
         void Create(TEntity obj);
-
         void Update(TEntity obj);
-
         bool Delete(int id);
     }
 }
